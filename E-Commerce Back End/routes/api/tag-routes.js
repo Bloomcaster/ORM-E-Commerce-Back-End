717ef5ab05e55c32eb0382f1a222f14a7661a851
Tahlia <tbolden13@gmail.com>
2022-11-15 16:33:06 -0800

    models/index finished

--- E-Commerce Back End/routes/api/tag-routes.js
+++ E-Commerce Back End/routes/api/tag-routes.js
@@ -2,23 +2,65 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
+// find a single tag by its `id`
   // be sure to include its associated Product data
+  router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(400).json({ message: 'There are no tags found with that id, please re-enter data' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+//create new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    if (req.body.productIds.length) {
+      const productTagIdArr = req.body.productIds.map( (product_id) => {
+        return {
+          product_id,
+          tag_id: tagData.id
+        };
+      })
+      const productTagData = await ProductTag.bulkCreate(productTagIdArr);
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
-router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  router.put('/:id', async (req, res) => {
+    try {
+      const tagData = await Tag.update(req.body, {
+        
+      })
+    }
 });
 
 router.delete('/:id', (req, res) => {
